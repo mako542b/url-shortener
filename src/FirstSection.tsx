c9654c8ca26efafc1b70b2c9cf12d88fc1e740e1
mako542b <marcin.piotr.konopka@gmail.com>
2022-12-06 00:33:13 +0100

    deploy gh-pages

--- src/FirstSection.tsx
+++ src/FirstSection.tsx
@@ -2,7 +2,7 @@ const FirstSection = () => {
     return (
         <div className="flex flex-col gap-8 pb-20 lg:flex-row-reverse lg:pl-24 xl:pl-40 lg:gap-10">
             <div className="overflow-hidden lg:w-1/2 grid items-center">
-                <img className="ml-6 -mr-30 lg:m-0 max-w-none max-h-80 lg:max-h-full object-cover w-header-image" src="../images/illustration-working.svg" alt="" />
+                <img className="ml-6 -mr-30 lg:m-0 max-w-none max-h-80 lg:max-h-full object-cover w-header-image" src="./images/illustration-working.svg" alt="" />
             </div>
             <div className="px-6 lg:px-0 text-center grid place-content-center lg:text-left gap-4 lg:w-1/2 lg:py-28">
                 <h1 className="text-head-one text-clr-Very-Dark-Blue font-bold lg:text-6xl xl:text-7xl xl:leading-very-tight lg:leading-very-tight">More than just shorter links</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://mako542b.github.io/url-shortener/',
   plugins: [react()],
   test: {
     environment: 'jsdom'
